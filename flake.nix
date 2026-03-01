a75bca8bf36a01cc70d922b01f38c31a6daa459d
Max <germanbluemax@gmail.com>
2026-03-01 09:48:28 -0800

    feat(dev-env): setup flake.nix

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Niko Tracker - GPS dog (or anything!) tracking system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell{
+          buildInputs = with pkgs; [
+            # Go backend
+            go
+            gopls
+
+            # Node/Typescript frontend
+            nodejs_22
+            nodePackages.typescript
+
+            # Protobuf tooling
+            protobuf
+            buf
+            protoc-gen-go
+            protoc-gen-go-grpc
+
+            # Database
+            postgresql_16
+            
+            # Utilities
+            jq
+            curl
+          ];
+
+          shellHook = ''
+            echo "Niko Tracker dev environment loaded"
+            echo "Go: $(go version)"
+            echo "Node: $(node --version)"
+            echo "Buf: $(buf --version)"
+          '';
+        };
+      });
+}
